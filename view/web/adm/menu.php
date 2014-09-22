b97d8d607bbca512e2136aa7cae983dba28ec224
willianoma <willianoma@hotmail.com>
2014-09-21 22:20:25 -0300

    subindo aplicação ok

---
+++ view/web/adm/menu.php
@@ -0,0 +1,24 @@
+<div id="menu" style="background: #afc9ec;
+     min-width: 774px;
+     position: relative;
+     height: content-box;
+     border-top: 2px solid #006cff;
+     border-bottom: 2px solid #006cff;">
+    <ul class="nav nav-tabs" role="tablist">           
+        <li><a href="index.php">Home</a></li>
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="?controller=Empresa&acao=listar">Empresas <span class="caret"></span> </a>
+        </li>
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="?controller=Usuario&acao=listar">Usuários <span class="caret"></span> </a>
+        </li>
+        <!--<li><a href="?controller=Usuario&acao=listar">Usuários</a></li>-->
+        <li><a href="?controller=Visita&acao=formCadastrarVisita">Cadastrar Visita</a></li>
+        <li><a href="?controller=Visita&acao=listar">Listar Visita</a></li>
+        <li><a href="?controller=BD&acao=configForm">Configurar Banco</a></li>
+        <li><a href="index.php?controller=Login&acao=logout">Logout</a> </li>
+
+
+
+    </ul>
+</div>
---
+++ view/web/user/menu.php
@@ -0,0 +1,24 @@
+
+<div id="menu" style="background: #afc9ec;
+     min-width: 774px;
+     position: relative;
+     height: content-box;
+     border-top: 2px solid #006cff;
+     border-bottom: 2px solid #006cff;">
+    <ul class="nav nav-tabs" role="tablist">           
+        <li><a href="index.php">Home</a></li>
+<!--        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="?controller=empresa&acao=listar">Empresas <span class="caret"></span> </a>
+        </li>
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="?controller=Usuario&acao=listar">Usuários <span class="caret"></span> </a>
+        </li>-->
+        <li><a href="?controller=Visita&acao=formCadastrarVisita">Cadastrar Visita</a></li>
+        <li><a href="?controller=Visita&acao=listar">Listar Visita</a></li>
+        <!--<li><a href="?controller=BD&acao=configForm">Configurar Banco</a></li>-->
+        <li><a href="index.php?controller=Login&acao=logout">Logout</a> </li>
+
+
+
+    </ul>
+</div>
